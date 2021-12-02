f668ab76f420c469d4b1414e3c761fd41596f577
Sultan Agabek <71389960+sula01@users.noreply.github.com>
2021-12-02 23:04:25 +0600

    Update GoldenAccount.java

--- week12/Task1/GoldenAccount.java
+++ week12/Task1/GoldenAccount.java
@@ -1,34 +1,51 @@
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class GoldenAccount extends Account {
 	public GoldenAccount(String number, String status, Client owner, BigDecimal balance) {
 		super(number, status, owner, balance);
 	}
 
-	@Override
 	public void debit(BigDecimal amount) {
-		BigDecimal newBalance = balance.subtract(amount);
+		BigDecimal newBalance = balance.subtract(amount).subtract(new BigDecimal("50.00"));
 
 		if (newBalance.compareTo(BigDecimal.ZERO) < 0) {
-			throw new IllegalArgumentException("balance can not be negative");
+			throw new IllegalArgumentException("Not enough money");
 		}
 
-		newBalance = newBalance.add(new BigDecimal("100.00"));
-
 		balance = newBalance;
-
-		operations[opIndex] = "Debit " + amount.toString();
-		opIndex++;
+		Operation operation = new Operation(
+				"Debit", amount, LocalDateTime.now()
+			);
+		operations.add(operation);
 	}
 
+	public void credit(BigDecimal amount) {
+		balance = balance.add(amount);
+		Operation op = new Operation(
+			"Credit",
+			amount,
+			LocalDateTime.now());
+		operations.add(op);
+	}
 
-	@Override
 	public void paymentForService() {
 		balance = balance.subtract(new BigDecimal("3000.00"));
 	}
 
-	@Override
-	protected String balanceBuilder() {
-		return "**** Your golden account balance is " + balance.toString() + "****";
+	public String balanceBuilder(){
+		return this.balance.toString();
+	}
+
+	public void getStatement() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Account statement\n");
+		for (Operation s: operations) {
+			if (s == null) {
+				break;
+			}
+			sb.append(s.toString()).append("\n");
+		}
+		System.out.println(sb);
 	}
 }
